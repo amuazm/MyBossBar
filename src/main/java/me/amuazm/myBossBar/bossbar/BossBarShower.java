db9e748376c0d94758369bb11c65a21c882bc695
amuazm <amuaz.mohd@gmail.com>
2024-11-19 07:03:20 +0800

    Revert ":bug: actually cancel task"
    This reverts commit 6fb41a539d51ce5b1fb912a1c53bafbaa8cd53c6.

--- src/main/java/me/amuazm/myBossBar/bossbar/BossBarShower.java
+++ src/main/java/me/amuazm/myBossBar/bossbar/BossBarShower.java
@@ -62,7 +62,6 @@ public class BossBarShower {
                     if (!trackedEntity.isValid() || !(trackedEntity.getChunk().isLoaded())) {
                         bossBarManager.stopTracking(trackedEntity);
                         nearbyAudience.hideBossBar(bossBar);
-                        scheduledTask.cancel();
                         return;
                     }
 
